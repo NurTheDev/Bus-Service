9b7896c8a71bd7dfff1e938e2fb5bc86d56da83f
NurTheDev <nur756.islam@gmail.com>
2024-12-26 00:32:31 +0600

    setup taildwind basic things

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,37 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        /* Neutral Colors */
+        gray: {
+          50: "#F7F8F8",
+          900: "#030712",
+        },
+        black: "#000000",
+        white: "#FFFFFF",
+
+        /* Primary Colors */
+        green: {
+          primary: "#1DD100",
+        },
+        blue: {
+          primary: "#4285F4",
+        },
+        red: {
+          primary: "#EA4335",
+        },
+        yellow: {
+          primary: "#FFBC04",
+        },
+      },
+      gradientColorStops: (theme) => ({
+        ...theme("colors"),
+        /* Custom Gradients */
+        "gradient-light": ["#030712 50%", "#000000 0%"],
+        "gradient-accent": ["#1DD100 40%", "#1DD100 15%", "#1DD100 10%"],
+      }),
+    },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
