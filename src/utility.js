f08b9508c4525f00ad1d980be1b0e4dde5e76348
NurTheDev <nur756.islam@gmail.com>
2024-12-31 17:25:38 +0600

    done some experiment

--- src/utility.js
+++ src/utility.js
@@ -3,5 +3,11 @@ let getseatLeft = () => {
   seatLeft.innerText = hi;
 };
 let getseatColor = (seat) => {
-  seat.style.backgroundColor = "red";
+  if (seat.classList.contains("bg-accentColor")) {
+    seat.classList.remove("bg-accentColor");
+    seat.classList.add("bg-primaryColor");
+  } else if (seat.classList.contains("bg-primaryColor")) {
+    seat.classList.remove("bg-primaryColor");
+    seat.classList.add("bg-accentColor");
+  }
 };
